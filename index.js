67d5a7bc13f5af8127984e5a66754b9ca2da6132
Scott Isebrand <scottisebrand@gmail.com>
2023-02-27 18:42:53 -0500

    Edited dueling-wizards-wireframe, index.js, and index.html

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
-let userHitPoints = 16;
-let computerHitPoints = 16;
+let userHealthLevel = 16;
+let computerHealthLevel = 16;
 let roundsRemaining = 5;
 
-// 
-function getComputerSpell () {
-  const arrayOfComputerSpells = ['earth', 'water', 'fire', 'wind'];
-  const randomNumber = Math.floor(Math.random() * 4);
-  const computerSpell = arrayOfComputerSpells[randomNumber];
-}
+// set eventListeners
+// const button = document.querySelectorAll('.button');
+  buttons.addEventListener('click', function (event)) {
+
+  }
+
+  blogPosts.forEach((blogPost) => {
+    // console.log(blogPost);
+    blogPost.addEventListener('mouseenter', function (event) {
+      // event.currentTarget.classList.toggle('purple');
+      event.target.classList.toggle('purple');
+      event.target.classList.toggle('red');
+      // }
+    });
+    blogPost.addEventListener('mouseleave', function (event) {
+      // if (!event.currentTarget.classList.toggle('purple')) {
+      event.target.classList.toggle('red');
+      event.target.classList.toggle('purple');
+      // }
+    });
+  });
+
+
+// game round function
+function playRound() {
+
+.addEventListener
+
+  function getUserSpell () {
+    const arrayOfUserSpells = ['water', 'fire', 'earth', 'wind'];
+    const computerSpell = arrayOfComputerSpells[randomNumber];
+    if 
+  }
+
+  function getComputerSpell () {
+    const arrayOfComputerSpells = ['water', 'fire', 'earth', 'wind'];
+    const randomNumber = Math.floor(Math.random() * 4);
+    const computerSpell = arrayOfComputerSpells[randomNumber];
+    if 
+  }
 
 
 }
+
